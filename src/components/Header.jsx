ca7cab03f384c7e31a9b920c016fa1eb3ae15fa5
sravan <sravan.k@egradtutor.in>
2023-07-20 13:27:39 +0530

    added nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import logo from "../assets/logo2.png"
+
+
+import "../styless/header.scss"
 
 const Header = () => {
     return (
         <>
             <header>
-                <div className="logo"><h1></h1></div>
-                <nav></nav>
-</header>
+                <nav className="navbar">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="nav-lists">
+                        <ul>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">about</a></li>
+                            <li><a href="#">serivces</a></li>
+                            <li><a href="#">faq's</a></li>
+                            <li><a href="#">contact</a></li>
+
+                        </ul>
+
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
